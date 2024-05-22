b861539712b039cb293a1c705fece9f08bc7265c
Llanz Kenneth Luy <luyllanzkenneth@gmail.com>
2024-05-22 21:15:55 +0800

    From local to MongoDB Atlas

--- config/database.mjs
+++ config/database.mjs
@@ -1,6 +1,6 @@
 // db.js
 import mongoose from 'mongoose';
-const DB_URI = 'mongodb://localhost:27017/iloilo-reserve-food'; // Change this to your MongoDB URI
+const DB_URI = 'mongodb+srv://luyllanzkenneth:kenjie321@cluster0.bkdwdn5.mongodb.net/iloilo-reserve-food?retryWrites=true&w=majority&appName=Cluster0'; // Change this to your MongoDB URI
 const PORT = process.env.PORT || 5000;
 
 export const connectDB = async () => {
@@ -12,3 +12,4 @@ export const connectDB = async () => {
     process.exit(1); // Exit the process with a non-zero status code
   }
 };
+// Kenjie@321
