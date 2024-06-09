6c2430b81383fb4eafad880a4ae74626a656d7fb
Llanz Kenneth Luy <luyllanzkenneth@gmail.com>
2024-06-09 15:17:07 +0800

    Change to timezone

--- utils/reservationUtils.mjs
+++ utils/reservationUtils.mjs
@@ -27,21 +27,25 @@ function convertToMinutes(time) {
 
 const checkReservationDateAndTime = async (restaurantObject, reservation_date, reservation_time) => {    
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDate = new Date(reservation_date);
-    const currentDayIndex = currentDate.getDay(); // 0 (Sunday) to 6 (Saturday)
+    
+    // Parse the reservation date and time with the Philippines timezone
+    const timezone = 'Asia/Manila';
+    const currentDate = moment.tz(reservation_date, timezone);
+    const currentDayIndex = currentDate.day(); // 0 (Sunday) to 6 (Saturday)
     const targetDay = days[currentDayIndex];
 
     const dayInfo = restaurantObject.openingHours.find(dayInfo => dayInfo.day === targetDay);
     if (!dayInfo || !dayInfo.isOpen) {
-        throw new Error(`Reservation day ${targetDay} is closed. Please choose other day`);
+        throw new Error(`Reservation day ${targetDay} is closed. Please choose another day.`);
     }
-    const reservationTimeInMinutes = convertToMinutes(reservation_time); 
-    const openTimeInMinutes = convertToMinutes(dayInfo.open); 
-    const closeTimeInMinutes = convertToMinutes(dayInfo.close);
-    const isWithinOperatingHours = reservationTimeInMinutes >= openTimeInMinutes && reservationTimeInMinutes <= closeTimeInMinutes;
-    if (!isWithinOperatingHours) {      
-      throw new Error(`Reservation time ${formatTimeTo12Hour(reservation_time)} is not within operating hours because the restaurant open from ${formatTimeTo12Hour(dayInfo.open)} to ${formatTimeTo12Hour(dayInfo.close)}`)
+    
+    const reservationDateTime = moment.tz(`${reservation_date} ${reservation_time}`, 'YYYY-MM-DD HH:mm', timezone);
+    const openTime = moment.tz(`${reservation_date} ${dayInfo.open}`, 'YYYY-MM-DD HH:mm', timezone);
+    const closeTime = moment.tz(`${reservation_date} ${dayInfo.close}`, 'YYYY-MM-DD HH:mm', timezone);
+    
+    if (!reservationDateTime.isBetween(openTime, closeTime, null, '[)')) {      
+      throw new Error(`Reservation time ${reservationDateTime.format('h:mm A')} is not within operating hours because the restaurant is open from ${openTime.format('h:mm A')} to ${closeTime.format('h:mm A')}`);
     }
-}
+};
 
 export { timeToMinutes, formatTimeTo12Hour, convertToMinutes, checkReservationDateAndTime };
